3412aefdfafc5a49de6e93b911b46727bb5359d5
Stefan Strigler <stefan@strigler.de>
2014-03-25 15:27:24 +0100

    always build jsjac

--- scripts/cplibs.sh
+++ scripts/cplibs.sh
@@ -2,9 +2,11 @@
 if [ ! -e ./lib/jsjac ]; then
   echo "jsjac is missing, checking out ..."
   git clone git://github.com/sstrigler/JSJaC.git lib/jsjac
-  echo "building jsjac ..."
-  make -C lib/jsjac
   echo "done"
 fi
+
+echo "building jsjac ..."
+make -C lib/jsjac nodoc
+echo "done"
 
 for i in ./lib/*; do for j in $i/*.{js,html}; do if [ -e "$j" ]; then cp "$j" ./htdocs; for k in ./htdocs.*; do cp "$j" "$k"; done ; fi; done; done;
